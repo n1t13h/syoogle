96c4a71d8d684033e1a2546bd6e922eb82654db9
Elliot Alderson <you@example.com>
2019-12-28 16:29:21 +0530

    made changes

--- main.py
+++ main.py
@@ -1,56 +1,36 @@
-# Imports
-import tkinter
-from ttkthemes import ThemedTk
-import sqlite3
-from datetime import datetime
-import os
-import tkinter.font as tkFont
-from tkinter.messagebox import showinfo, showerror
-from tkinter import simpledialog
 import tkinter as tk
-from tkscrolledframe import ScrolledFrame
-from tkinter import *
-import csv
-from tkinter import ttk
-from tkcalendar import DateEntry
-from fpdf import FPDF
-import webbrowser
-import babel.numbers
-import random
-import ctypes
-import sys
 
 
-# COLORS
-MAIN_UI_COLOR = "#9556eb"
-FRAME_BACKGROUND = random.choice(["#ffffff"])
-MAIN_RED = "#808e9b"
-path = os.path.normpath(os.path.expanduser("~/Documents/Canteen"))
-dbPath = "canteen.db"
+LARGE_FONT= ("Verdana", 12)
 
 
-def initialize_font():
-    if "Open Sans" in tkFont.families():
-        default_font = tkFont.nametofont("TkDefaultFont")
-        default_font.configure(family="Arial", size=12)
+class Syoogle(tk.Tk):
 
-        default_font = tkFont.nametofont("TkTextFont")
-        default_font.configure(family="Arial", size=12)
-
-
-class Canteen(tk.Tk):
     def __init__(self, *args, **kwargs):
-
+        
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
-        container.grid(row=0, column=0, sticky="NEWS")
+        self.title("{ SynTech-X '19 - '20 }")
+        # Gets the requested values of the height and widht.
+        windowWidth = self.winfo_reqwidth()
+        windowHeight = self.winfo_reqheight()
+        print("Width",windowWidth,"Height",windowHeight)
+ 
+        # Gets both half the screen width/height and window width/height
+        positionRight = int(self.winfo_screenwidth()/2 - windowWidth/2)
+        positionDown = int(self.winfo_screenheight()/2 - windowHeight/2)
+ 
+        # Positions the window in the center of the page.
+        self.geometry("+{}+{}".format(positionRight, positionDown))
+
+        container.grid(row=0,column=0)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in [
-            PageOne,
-            Home
-        ]:
+        for F in (StartPage, PageOne, PageTwo):
 
             frame = F(container, self)
 
@@ -58,73 +38,67 @@ class Canteen(tk.Tk):
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(Home)
+        self.show_frame(StartPage)
 
     def show_frame(self, cont):
 
         frame = self.frames[cont]
         frame.tkraise()
 
-    def update_frame(self, page_name):
-        frame = self.frames[page_name]
-        frame.update()
-        self.show_frame(page_name)
+        
+class StartPage(tk.Frame):
 
-    # def show_frame(self, page_name):
-    #     for frame in self.frames.values():
-    #         frame.grid_remove()
-    #     frame = self.frames[page_name]
-    #     frame.grid()
-    def center_window(self):
-        # get screen width and height
-        ws = self.winfo_screenwidth()  # width of the screen
-        hs = self.winfo_screenheight()  # height of the screen
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Syoogle", font=LARGE_FONT)
+        label.grid(row=0,column=0)
 
-        # set the dimensions of the screen
-        # and where it is placed
-        self.geometry("%dx%d" % (ws, hs))
+        button = tk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.grid(row=1,column=0)
 
+        button2 = tk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.grid(row=1,column=1)
 
 
 class PageOne(tk.Frame):
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.config(bg=FRAME_BACKGROUND)
-        lbl = Label(text="syoogle")
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.grid(row=0,column=0)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.grid(row=1,column=0)
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.grid(row=2,column=0)
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        lbl = tk.Label(self,text="SYOOGLE")
         lbl.grid(row=0,column=0,padx=5,pady=5)
         lbl.configure(background="#8000ff",foreground="#ffffff",font="Arial 40",relief="flat")
 
-        lbl1 = Label(text = "ENTER COLLEGE NAME / COLLEGE CODE / TEAM NAME")
+        lbl1 = tk.Label(self,text = "ENTER COLLEGE NAME / COLLEGE CODE / TEAM NAME")
         lbl1.grid(row=1,column=0,padx=15,pady=15)
         lbl1.configure(background="#ffffff",foreground="#000000",font="-family {Arial Black} -size 24 -weight bold -slant italic",relief="flat")
 
-        self.collegeCode = ttk.Entry()
+        self.collegeCode = tk.Entry(self)
         self.collegeCode.grid(row=2,column=0,padx=5,pady=5)
         self.collegeCode.configure(background="#ffffff",foreground="#8000ff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
 
-        submitButton = Button(command=lambda:controller.show_frame(PageOne),text="SUBMIT")
+        submitButton = tk.Button(self,command=lambda:controller.show_frame(PageOne),text="SUBMIT")
         submitButton.configure(background="#000000",foreground="#ffffff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
         submitButton.grid(row=3,column=0,padx=5,pady=5)
-class Home(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.config(bg=FRAME_BACKGROUND)
-        Label(self,text="MANAGE YOUR ITEM INVENTORY!!!").grid(row=0,column=0,sticky="NEWS",columnspan=4,pady=50)
-        Button(self,text="INVENTORY MANAGER",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(ipadx=100,columnspan=4,row=1,column=0)
-        Label(self,text="Manage Your Purchase And Sales !!!").grid(row=2,column=0,sticky="NEWS",columnspan=4,pady=50)
-        Button(self,text="View Sales Data",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(row=3,column=1,padx=80,ipady=20)
-        Button(self,text="Insert Sales Data",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(row=3,column=0,padx=80,ipady=20)
-        Button(self,text="Conversion",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(row=3,column=2,padx=80,ipady=20)
-        Button(self,text="Manage Expenses",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(row=3,column=3,padx=80,ipady=20)
-        Label(self,text="Generate Reports!!!").grid(row=4,column=0,sticky="NEWS",columnspan=4,pady=50)
-        Button(self,text="Generate / Manage Reports",background=MAIN_UI_COLOR,command=lambda:controller.show_frame(PageOne)).grid(row=5,ipady=20,column=0,columnspan=4)
+        
 
-app = Canteen() 
-app.wm_title("Canteen Manager")
-initialize_font()
+
+app = Syoogle()
 app.mainloop()
-    
-
-
-
-
--- test.py
+++ test.py
@@ -2,18 +2,18 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        lbl = Label(text="syoogle")
+        lbl = Label(self,text="syoogle")
         lbl.grid(row=0,column=0,padx=5,pady=5)
         lbl.configure(background="#8000ff",foreground="#ffffff",font="Arial 40",relief="flat")
 
-        lbl1 = Label(text = "ENTER COLLEGE NAME / COLLEGE CODE / TEAM NAME")
+        lbl1 = Label(self,text = "ENTER COLLEGE NAME / COLLEGE CODE / TEAM NAME")
         lbl1.grid(row=1,column=0,padx=15,pady=15)
         lbl1.configure(background="#ffffff",foreground="#000000",font="-family {Arial Black} -size 24 -weight bold -slant italic",relief="flat")
 
-        self.collegeCode = ttk.Entry()
+        self.collegeCode = Entry(self)
         self.collegeCode.grid(row=2,column=0,padx=5,pady=5)
         self.collegeCode.configure(background="#ffffff",foreground="#8000ff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
 
-        submitButton = Button(command=lambda:controller.show_frame(PageOne),text="SUBMIT")
+        submitButton = Button(self,command=lambda:controller.show_frame(PageOne),text="SUBMIT")
         submitButton.configure(background="#000000",foreground="#ffffff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
         submitButton.grid(row=3,column=0,padx=5,pady=5)
