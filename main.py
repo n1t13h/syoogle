74405c22a3e387c1bead9c06669c90ec77dfef0e
nitishpatel <nitishp74@gmail.com>
2019-12-28 21:50:07 +0530

    a lot of changes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
-
-
+from datetime import datetime
+from tkinter import messagebox
 LARGE_FONT= ("Verdana", 12)
 
+#VARIABLES
+que1 = "Who was the first Marvel hero to get a sequel?"
+ans1 = "ironman"
+clue1 = "U2FsdGVkX1/kfrltdoLLKU+OSSRBFz8TTMw+yEOpMTI=" #3DES
+clue1Ans = "Avengers"
 
 class Syoogle(tk.Tk):
 
@@ -50,33 +55,90 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Syoogle", font=LARGE_FONT)
-        label.grid(row=0,column=0)
+        def onSubmit():
+            if(len(self.collegeCode.get())>0):
+                file = open("game.txt","w")
+                txt = self.collegeCode.get() + " started syoogle at " + str(datetime.now())
+                file.write(txt)
+                file.close()
+                controller.show_frame(PageOne)
+            else:
+                messagebox.showerror("Error","You Kidding?? Enter The Code!!!")
 
-        button = tk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
-        button.grid(row=1,column=0)
+        lbl = tk.Label(self,text="SYOOGLE")
+        lbl.grid(row=0,column=0,padx=5,pady=5)
+        lbl.configure(background="#8000ff",foreground="#ffffff",font="Arial 40",relief="flat")
 
-        button2 = tk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.grid(row=1,column=1)
+        lbl1 = tk.Label(self,text = "ENTER COLLEGE NAME / COLLEGE CODE / TEAM NAME")
+        lbl1.grid(row=1,column=0,padx=15,pady=15)
+        lbl1.configure(background="#ffffff",foreground="#000000",font="-family {Arial Black} -size 24 -weight bold -slant italic",relief="flat")
+
+        self.collegeCode = tk.Entry(self)
+        self.collegeCode.grid(row=2,column=0,padx=5,pady=5)
+        self.collegeCode.configure(background="#ffffff",foreground="#8000ff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
+
+        submitButton = tk.Button(self,command=onSubmit,text="SUBMIT")
+        submitButton.configure(background="#000000",foreground="#ffffff",font="-family {Arial Black} -size 20 -weight bold -slant italic")
+        submitButton.grid(row=3,column=0,padx=5,pady=5)
 
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
-        label.grid(row=0,column=0)
+        def checkAnswer():
+            if(self.ans.get()==ans1):
+                self.ans.configure(foreground="#27ae60")
+                self.ans.config(state="readonly")
+                
+                clue.config(state="normal")
+                clue.insert(0,clue1)
+                clue.config(state="readonly")
 
-        button1 = tk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.grid(row=1,column=0)
+                lbl2['text']="DECRYPT THE TEXT !! Is That 3DES or ShiftCipher?"
+                cluechkButton.config(state=tk.ACTIVE)
 
-        button2 = tk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.grid(row=2,column=0)
+            else:
+                self.ans.configure(foreground="#ff0000")
+                
 
+        def checkClue():
+            if(clueAns.get()==clue1Ans):
+                pass
+            else:
+                pass
+
+
+
+        lbl1 = tk.Label(self,text = que1)
+        lbl1.grid(row=0,column=0,padx=15,pady=15,columnspan=3)
+        lbl1.configure(background="#ffffff",foreground="#000000",font="-family {Arial Black} -size 24 -weight bold -slant italic",relief="flat")
+        
+        self.ans = tk.Entry(self)
+        self.ans.grid(row=1,column=0,padx=5,pady=5)
+        self.ans.configure(background="#f7f1e3",foreground="#2c2c54",font="-family {Arial Black} -size 20 -weight bold -slant italic")
+
+        chkButton = tk.Button(self,text="✓",command=checkAnswer)
+        chkButton.grid(row=1,column=1)
+        chkButton.configure(background="#000000",foreground="#33d9b2",font="-family {Arial Black} -size 12 -weight bold -slant italic")
+
+
+
+        clue = tk.Entry(self,width=20)
+        clue.grid(row=1,column=2,padx=5,pady=5,ipadx=100)
+        clue.config(state="disabled")
+        clue.configure(background="#ffffff",foreground="#c0392b",font="-family {Arial Black} -size 12 -weight bold -slant italic")
+        
+        clueAns = tk.Entry(self)
+        clueAns.grid(row=3,column=0,padx=5,pady=5,ipadx=100,columnspan=3)
+        clue.configure(background="#ffffff",foreground="#c0392b",font="-family {Arial Black} -size 12 -weight bold -slant italic")
+
+        lbl2 = tk.Label(self,text="")
+        lbl2.grid(row=2,column=2,padx=5,pady=5)
+
+        cluechkButton = tk.Button(self,text="Submit",command=checkAnswer,state=tk.DISABLED)
+        cluechkButton.grid(row=4,column=0,columnspan=3)
+        cluechkButton.configure(background="#000000",foreground="#33d9b2",font="-family {Arial Black} -size 12 -weight bold -slant italic")
 
 class PageTwo(tk.Frame):
 
